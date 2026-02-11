aa22a366324f2e6de5112356ec5ff9f1df81f6ee
fajeribrahim <fajeribrahim64@gmail.com>
2026-02-11 11:29:12 +0700

    feat: cart pt2

--- src/components/CardListHorizontal.jsx
+++ src/components/CardListHorizontal.jsx
@@ -1,12 +1,16 @@
-import { Card } from "flowbite-react";
+import { Card, ButtonGroup, Button } from "flowbite-react";
+import { useContext } from "react";
+import { FaMinus, FaPlus, FaTrash } from "react-icons/fa";
+import { CartContext } from "../contexts/CartContext";
 
 const CardListHorizontal = ({ data }) => {
+    const { changeQty, deleteAll, deleteItem } = useContext(CartContext);
     return (
         <div>
             <Card className="block mx-auto my-25 w-4xl">
                 <div className="mb-4 flex items-center justify-between">
                     <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Keranjang</h5>
-                    <a className="text-sm font-medium text-red-600 hover:underline dark:text-red-500">
+                    <a className="text-sm font-medium text-red-600 hover:underline dark:text-red-500" onClick={deleteAll}>
                         Hapus Semua
                     </a>
                 </div>
@@ -29,6 +33,14 @@ const CardListHorizontal = ({ data }) => {
                                             <p className="truncate text-sm text-gray-500 dark:text-gray-400 font-bold">X{item.qty}</p>
                                         </div>
                                         <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">${item.price}</div>
+                                    </div>
+                                    <div className="flex justify-end mt-4">
+                                        <ButtonGroup>
+                                            <Button color="alternative"><FaMinus className="w-2 h-2" onClick={() => changeQty(item.id, "-")} /></Button>
+                                            <Button color="alternative">{item.qty}</Button>
+                                            <Button color="alternative"><FaPlus className="w-2 h-2" onClick={() => changeQty(item.id, "+")} /></Button>
+                                        </ButtonGroup>
+                                        <FaTrash className="text-2xl ms-4 mt-2 text-red-500" onClick={() => deleteItem(item.id)}/>
                                     </div>
                                 </li>
                             ))
--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -13,7 +13,7 @@ export default function CartProvider({ children }) {
                 // looping data cart, temukan id yang dimaksud, update qty nya
                 return prev.map((item) => {
                     if (item.id == product.id) {
-                        return {...item, qty: item.qty + qty}
+                        return { ...item, qty: item.qty + qty }
                     } else {
                         return item;
                     }
@@ -33,13 +33,45 @@ export default function CartProvider({ children }) {
         })
     }
 
+    function changeQty(productId, type) {
+        setCart((prev) => {
+            // loop data cart, cari data id yang sesuai, update qty nya
+            return prev.map((item) => {
+                // jika id yg di loop sm dgn productId yg dicari
+                if (item.id = productId) {
+                    if (type == "+") {
+                        return { ...item, qty: item.qty + 1 }
+                    } else {
+                        // jk tipe perubahannya -, diupdate max diangka 1 kurangny
+                        if (item.qty > 1) {
+                            return { ...item, qty: item.qty - 1 }
+                        }
+                    }
+                } else {
+                    return item; // jika id loop bkn productId diminta, tdk diubah
+                }
+            })
+        })
+    }
+
+    function deleteItem(productId) {
+        setCart((prev) => {
+            // ambil item yg id ny selain yg diminta, hilangkan yg diminta
+            return prev.filter((item) => item.id != productId);
+        })
+    }
+
+    function deleteAll() {
+        setCart([]);
+    }
+
     // debugin, cek data cart masuk atau tidak
-    useEffect(() => {
-        console.log(cart);
-    },[cart])
+    // useEffect(() => {
+    //     console.log(cart);
+    // },[cart])
 
     return (
-        <CartContext.Provider value={{ cart, addToCart }}>
+        <CartContext.Provider value={{ cart, addToCart, changeQty, deleteAll, deleteItem }}>
             {children}
         </CartContext.Provider>
     )
